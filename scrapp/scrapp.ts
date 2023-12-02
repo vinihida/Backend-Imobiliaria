7b9f4ae273847398f0bbbd5394fa495a18e0261d
Vinicius Hidalgo <hidalgo.vinicius3@gmail.com>
2023-12-02 11:39:23 -0300

    Add scrapp function for dummy random address

---
+++ scrapp/scrapp.ts
@@ -0,0 +1,16 @@
+var Numero = ["0", "1442", "4614", "4725", "4532", "1567", "1508", "4611", "4531", "0535", "4405", "3480", "1156"];
+var Nome = ["Rua da pedra", "Avenida General Osório", "Rua Brigadeiro Faria Lima", "Rua Pedro Noveleto", "Avenida Washington", "Avenida Sabugo de Milho"];
+var Cidade = ["Campinas", "Sumaré", "Hortolândia", "Americana", "Nova Odessa", "Caraguatatuba", "Mauá"];
+var Estado = ["São Paulo", "Rio de Janeiro", "Mato Grosso do Sul", "Minas Gerais", "Paraiba"];
+var Cep = ["000340645", "000179872", "000439386", "000514657", "000993181", "000633740"];
+
+var template = [Numero, " ", Nome, ", ", Cidade, " ", Estado, ", ", Cep];
+
+function getRandomAddress() {
+  return template.map(getRandomElement).join("");
+}
+
+function getRandomElement(array: string | String[]) {
+  if (array instanceof Array) return array[Math.floor(Math.random() * array.length)];
+  else return array;
+};
